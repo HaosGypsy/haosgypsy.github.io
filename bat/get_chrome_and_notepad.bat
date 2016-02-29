e2bbcd4222d6b7e7cdcf5c8069bccf7982aa234b
Omni <haosgypsy@gmail.com>
2016-02-29 15:50:36 +0900

    Update get_chrome_and_notepad.bat

--- bat/get_chrome_and_notepad.bat
+++ bat/get_chrome_and_notepad.bat
@@ -4,6 +4,11 @@ cd portable_apps
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://jaist.dl.sourceforge.net/project/portableapps/Google%%20Chrome%%20Portable/GoogleChromePortable_48.0.2564.109_online.paf.exe', 'GoogleChromePortable_48.0.2564.109_online.paf.exe')"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://jaist.dl.sourceforge.net/project/portableapps/Notepad%%2B%%2B%%20Portable/NotepadPlusPlusPortable_6.8.8.paf.exe', 'NotepadPlusPlusPortable_6.8.8.paf.exe')"
 start GoogleChromePortable_48.0.2564.109_online.paf.exe
-pause
+rem pause
 start NotepadPlusPlusPortable_6.8.8.paf.exe
+rem pause
+cd ..
+(
+  echo start %~dp0portable_apps\GoogleChromePortable\googleChromePortable.exe --lang=en
+) > run_chrome_in_english.bat
 pause
